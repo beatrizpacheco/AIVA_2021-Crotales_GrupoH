377afb0b0fa007d945b8ef3060a82bbca954d0cc
beatrizpacheco <32259114+beatrizpacheco@users.noreply.github.com>
2021-05-01 13:26:19 +0200

    #5

--- EstadisticasReconocimiento.py
+++ EstadisticasReconocimiento.py
@@ -7,6 +7,7 @@ class MakeTests():
         self.recogniser = main
         self.sucess = 0
         self.test = 0
+        self.images_succes = []
         csv_reader = pd.read_excel('./CrotalesDB/GroundTruth.ods',  header=0, converters={'NUM_DOCUMENTO': str, 'Real': str})
         for row in csv_reader.values:
             file_name = row[0]
@@ -17,6 +18,7 @@ class MakeTests():
             groundtruth = row[1]
             self._procesar(file_name, groundtruth)
         print("la tasa de acierto es de ", self.sucess/self.test)
+        print("las imagenes acertadas son: ", self.images_succes)
 
     def _procesar(self, file_name, groundtruth):
         print("./CrotalesDB/TestSamples/" + file_name)
@@ -25,6 +27,7 @@ class MakeTests():
         self.test += 1
         if result == groundtruth:
             self.sucess +=1
+            self.images_succes.append(file_name)
 
 if __name__ == "__main__":
     m = MakeTests()
