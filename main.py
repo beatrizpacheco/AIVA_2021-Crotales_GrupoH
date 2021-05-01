3397ba524310c9ccdf3ee80ce1824590c8d5e442
beatrizpacheco <32259114+beatrizpacheco@users.noreply.github.com>
2021-05-01 13:26:06 +0200

    #6

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import cv2
 import Preprocesador
-import detectorSecuenciaPrincipal
+import detectorSecuencia
 import ReconocedorNumeros
+import os
 
 
-def prueba(imagen_in):
+def prueba(imagen_in): #, filename):
 
     #preprocesador
-    imagen_prep = Preprocesador.preprocesador(imagen_in)
-
+    imagen_prep_org, imagen_prep_umb = Preprocesador.preprocesador(imagen_in)
+    '''
+    path = "resultado"
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    filename = path + "/" + filename
+    cv2.imwrite(filename, imagen_prep_umb)
+    '''
     #detector
-    pos = detectorSecuenciaPrincipal.detector(imagen_prep)
+    string_org, string_umb, data_org, data_umb = detectorSecuencia.detector(imagen_prep_org, imagen_prep_umb)
 
     #reconocedor
-    numbers = ReconocedorNumeros.reconocedor(imagen_prep, pos)
+    numbers = ReconocedorNumeros.reconocedor(string_org, string_umb, data_org, data_umb)
     return numbers
 
 if __name__ == "__main__":
     # llamamos imread()
     imagen_in = cv2.imread("./CrotalesDB/TestSamples/0073.TIF")
-    prueba(imagen_in)
+    crotal = prueba(imagen_in)
+    print("el numero de crotal es: ", crotal)
