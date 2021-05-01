321f15eb20298c51b2921455c3df53375c587c53
beatrizpacheco <32259114+beatrizpacheco@users.noreply.github.com>
2021-05-01 13:25:44 +0200

    #3

--- ReconocedorNumeros.py
+++ ReconocedorNumeros.py
@@ -1,14 +1,48 @@
+def get_number_data(dato):
+    contador = 0
+    numbers = ""
+    for _, j in enumerate(dato[::-1]):
+        if j.isnumeric():
+            numbers = numbers + j
+            contador += 1
+            if contador == 4:
+                break
+    numbers = numbers[::-1]
+    return numbers
 
-def reconocedor(img, string):
+
+def get_number_str(dato):
+    contador = 0
+    numbers = ""
+    for i in reversed(dato):
+        if i.isnumeric():
+            numbers = numbers + i
+            contador += 1
+            if contador == 4:
+                break
+    numbers = numbers[::-1]
+
+    return numbers
+
+
+
+def reconocedor(string_org, string_umb, data_org, data_umb):
     '''
     DEVOLVEMOS LOS NÚMEROS DE LAS ÚLTIMAS POSICIONES
     PENDIENTE MEJORAR MÉTODO ANTERIOR CON IMAGEN Y SU POSICIÓN
     '''
 
-    numbers = ""
+    numbers = get_number_data(data_umb)
+    if len(numbers) < 4 or len(numbers) > 5:
+        numbers = get_number_data(data_org)
 
-    for i in string[-9:]:
-        if i.isnumeric():
-            numbers = numbers + i
-    #print(numbers)
+    if len(numbers) < 4 or len(numbers) > 5:
+        numbers = get_number_str(string_umb)
+
+    if len(numbers) < 4 or len(numbers) > 5:
+        numbers = get_number_str(string_org)
+
+    #print("el numero de crotal es: ", numbers)
+
+
     return numbers
