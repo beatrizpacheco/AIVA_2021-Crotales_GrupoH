aa54e90c3174ccae9ec4deacfc921f593a8fb20c
SergioUcles <74303284+SergioUcles@users.noreply.github.com>
2021-05-05 15:29:25 +0200

    #4

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -4,10 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ProcessBuilder builder = new ProcessBuilder("C:\\EjemploAI\\main.bat");
+        //Introducir la ruta global de la imagen del crotal
+        String image_crotal = new String("C:\\EjemploAI\\AIVA_2021-Crotales_GrupoH\\TestSamples/0001.TIF");
+        ProcessBuilder builder = new ProcessBuilder("C:\\EjemploAI\\main.bat",image_crotal);
         Process process = builder.start();
 
         InputStream stdout = process.getInputStream();
