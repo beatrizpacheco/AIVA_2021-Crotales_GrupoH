a29b9b6d58a737f4fafe8f26e5e2a00dad42f50f
beatrizpacheco <32259114+beatrizpacheco@users.noreply.github.com>
2021-05-09 10:17:24 +0200

    #6

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import argparse
 import NumCrotalLib
 
 if __name__ == "__main__":
-
+    '''
     ap = argparse.ArgumentParser()
     ap.add_argument("--image_crotal", required=True, help="path to where the images files resides")
 
     args = vars(ap.parse_args())
     path_image = args['image_crotal']
-
-    #imagen_in = cv2.imread("C:\EjemploAI\AIVA_2021-Crotales_GrupoH\TestSamples/0001.TIF")
-    imagen_in = cv2.imread(path_image)
+    '''
+    imagen_in = cv2.imread("/usr/src/app/CrotalesDB/TestSamples/0002.TIF")
+    #imagen_in = cv2.imread(path_image)
     crotal = NumCrotalLib.reconocedor_crotal(imagen_in)
     print("el numero de crotal es: ", crotal)
