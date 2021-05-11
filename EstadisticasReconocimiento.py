e468ba02b4f43e90e7aca13d01710718cc04e6ac
beatrizpacheco <32259114+beatrizpacheco@users.noreply.github.com>
2021-05-11 16:14:20 +0200

    #5

--- EstadisticasReconocimiento.py
+++ EstadisticasReconocimiento.py
@@ -5,9 +5,12 @@ import main
 class MakeTests():
     def __init__(self):
         self.recogniser = main
-        self.sucess = 0
-        self.test = 0
-        self.images_succes = []
+        self.tp = 0 # acierto
+        self.fn = 0 # no detecto números
+        self.fp = 0 # detecto números incorrectos
+        self.test = 0 # imágenes totales
+
+        #self.images_succes = []
         csv_reader = pd.read_excel('./CrotalesDB/GroundTruth.ods',  header=0, converters={'NUM_DOCUMENTO': str, 'Real': str})
         for row in csv_reader.values:
             file_name = row[0]
@@ -17,8 +20,16 @@ class MakeTests():
             file_name = file_name + ".TIF"
             groundtruth = row[1]
             self._procesar(file_name, groundtruth)
-        print("la tasa de acierto es de ", self.sucess/self.test)
-        print("las imagenes acertadas son: ", self.images_succes)
+        print("la tasa de acierto es de ", self.tp/self.test)
+        precision = round(self.tp / (self.tp + self.fp), 3)
+        print("precision es: ", precision)
+        recall = round(self.tp / (self.tp + self.fn), 3)
+        print("recall es: ", recall)
+        iou = round(self.tp / (self.fn + self.tp + self.fp),3)
+        print("IoU es: ", iou)
+        f1 = round((2 * precision * recall) / (precision + recall), 3)
+        print("f1 es: ", f1)
+        #print("las imagenes acertadas son: ", self.images_succes)
 
     def _procesar(self, file_name, groundtruth):
         print("./CrotalesDB/TestSamples/" + file_name)
@@ -26,8 +37,13 @@ class MakeTests():
         result = self.recogniser.NumCrotalLib.reconocedor_crotal(img)
         self.test += 1
         if result == groundtruth:
-            self.sucess +=1
-            self.images_succes.append(file_name)
+            self.tp +=1
+            #self.images_succes.append(file_name)
+        elif result == "":
+            self.fn += 1
+        elif result != groundtruth:
+            self.fp += 1
+
 
 if __name__ == "__main__":
     m = MakeTests()
